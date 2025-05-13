8f3e4981b8ce46293c256e49318a7ade9f36cd34
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-13 17:26:56 +0200

    Exercice 1 Etape 3

--- src/photoloader.js
+++ src/photoloader.js
@@ -1,4 +1,4 @@
-import { url } from "./config.js";
+import {domain, url} from "./config.js";
 
 export function loadPicture(idPicture) {
     return fetch(`${url}/photos/${idPicture}`, {credentials: 'include'}).then((response) => {
@@ -8,5 +8,16 @@ export function loadPicture(idPicture) {
         return response.json();
     }).catch((error) => {
         console.error("Error fetching picture:", error);
+    })
+}
+
+export function loadResource(uri) {
+    return fetch(`${domain}${uri}`, {credentials: 'include'}).then((response) => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    }).catch((error) => {
+        console.error("Error fetching resource:", error);
     })
 }
