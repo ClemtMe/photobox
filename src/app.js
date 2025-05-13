46f0c97550cba1b45d4a8402c718e78e19f1e558
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2025-05-13 17:26:56 +0200

    Exercice 1 Etape 3

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-import {loadPicture} from "./photoloader.js";
-import {displayPicture} from "./ui";
+import {loadPicture, loadResource} from "./photoloader.js";
+import {displayCategory, displayComments, displayPicture} from "./ui";
 
 async function getPicture(id) {
     let data = await loadPicture(id);
     console.log(data);
     displayPicture(data.photo);
+    getCategory(data);
+}
+
+async function getCategory(pictureData){
+    let data = await loadResource(pictureData.links.categorie.href);
+    console.log(data);
+    displayCategory(data.categorie);
+    getComments(pictureData);
+}
+
+async function getComments(pictureData){
+    let data = await loadResource(pictureData.links.comments.href);
+    console.log(data);
+    displayComments(data.comments);
 }
 
 let numero = window.location.hash.replace("#", "");
--- src/ui.js
+++ src/ui.js
@@ -7,3 +7,18 @@ export function displayPicture(photo) {
     photo.url = domain+photo.url.href;
     document.querySelector("#la_photo").innerHTML = templateFunction(photo);
 }
+
+export function displayCategory(category) {
+    const templateSource = document.querySelector("#la_categorie_template").innerHTML
+    const templateFunction = Handlebars.compile(templateSource);
+    document.querySelector("#la_categorie").innerHTML = templateFunction(category);
+}
+
+export function displayComments(comments) {
+    const templateSource = document.querySelector("#les_commentaires_template").innerHTML
+    const templateFunction = Handlebars.compile(templateSource);
+    comments.forEach(comment => {
+        comment.content = comment.content.replace(/&#39;/g, "'");
+    })
+    document.querySelector("#les_commentaires").innerHTML = templateFunction({commentaires: comments});
+}
