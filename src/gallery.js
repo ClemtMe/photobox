8cb160b1a6bc9aa913c663ab188d0f6eb1e9e3c0
Kevin Ngo <kevin.ngo8@etu.univ-lorraine.fr>
2025-05-20 15:45:59 +0200

    ex2 changer de page

--- src/gallery.js
+++ src/gallery.js
@@ -1,6 +1,6 @@
 import {loadPicture} from "./photoloader";
 import {url} from "./config";
-
+import {display_galerie} from "./gallery_ui";
 
 
 export async function load() {
@@ -27,5 +27,23 @@ function promesse(){
         return response.json();
     }).catch((error) => {
         console.error("Error fetching picture:", error);
+    });
+}
+
+export function next(){
+    const nextButton = document.querySelector("#next");
+    nextButton.addEventListener("click", async () => {
+        const data = await load();
+        data.page++;
+        display_galerie(data);
+    });
+}
+
+export function prev(){
+    const prevButton = document.querySelector("#prev");
+    prevButton.addEventListener("click", async () => {
+        const data = await load();
+        data.page--;
+        display_galerie(data);
     });
 }
